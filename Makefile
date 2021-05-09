3e74738683e33d9a43611cc4df7369b5a2e5af7e
kingcobra2468 <eriktyryshkin@gmail.com>
2021-05-08 23:06:24 -0700

    added unit tests

--- Makefile
+++ Makefile
@@ -1,11 +1,15 @@
 BUILD_DIR 	= bin/
 CC        	= g++
-CFLAGS    	= -fPIC -Wall -Werror -Ibase64/src -Ibase64/include
+CFLAGS    	= -Wall -Werror -Ibase64/src -Ibase64/include -o2
+SO_CFLAGS   = -fPIC
+TEST_CFLAGS = -static -Lbin/ -lbase64 -Ilibs/
 LDFLAGS     = -shared
 LIB       	= libbase64
+TEST_LIB    = $(LIB)_tests
 STATIC    	= $(BUILD_DIR)$(LIB).a
 SHARED_LIB  = $(BUILD_DIR)$(LIB).so
 SRC         = $(wildcard base64/src/*.cpp)
+TEST_SRC    = $(wildcard tests/*.cpp)
 OBJ         = $(SRC:.cpp=.o)
 
 all: $(STATIC) $(SHARED_LIB)
@@ -17,10 +21,13 @@ $(STATIC): $(OBJ)
 	$(AR) $(ARFLAGS) $@ $(addprefix $(BUILD_DIR), $(notdir $^))
 
 %.o: %.cpp build
-	$(CC) $(CFLAGS) -c -o $(BUILD_DIR)$(notdir $@) $<
+	$(CC) $(CFLAGS) $(SO_CFLAGS) -c -o $(BUILD_DIR)$(notdir $@) $<
 
 build:
 	mkdir -p $(BUILD_DIR)
+
+test: $(STATIC)
+	$(CC) $(CFLAGS) $(TEST_SRC) -o  $(BUILD_DIR)$(TEST_LIB) $(TEST_CFLAGS)
 
 .PHONY: clean
 clean:
---
+++ tests/tests.cpp
@@ -0,0 +1,27 @@
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include <base64.hpp>
+#include <string>
+
+TEST_CASE("Base64 string encode: 1", "[encode]")
+{
+    REQUIRE_THAT(base64::base64_encode("test"), Catch::Matchers::Equals("dGVzdA=="));
+}
+
+TEST_CASE("Base64 string encode: 2", "[encode]")
+{
+    REQUIRE_THAT(base64::base64_encode("This is a sentence. Special chars !@#$%^&*()"),
+                 Catch::Matchers::Equals("VGhpcyBpcyBhIHNlbnRlbmNlLiBTcGVjaWFsIGNoYXJzICFAIyQlXiYqKCk="));
+}
+
+TEST_CASE("Base64 string decode: 1", "[decode]")
+{
+    REQUIRE_THAT(base64::base64_decode("VGhpcyBpcyBhIHNlbnRlbmNlLiBTcGVjaWFsIGNoYXJzICFAIyQlXiYqKCk="),
+                 Catch::Matchers::Equals("This is a sentence. Special chars !@#$%^&*()"));
+}
+
+TEST_CASE("Base64 string decode: 2", "[decode]")
+{
+    REQUIRE_THAT(base64::base64_decode("VGhpcyBpcyBhIHNlbnRlbmNlLiBTcGVjaWFsIGNoYXJzICFAIyQlXiYqKCk="),
+                 Catch::Matchers::Equals("This is a sentence. Special chars !@#$%^&*()"));
+}
